a1c494a506eb1a1c196487baae2a0ba999c38551
zhangzejun3 <zzj2472@163.com>
2018-11-28 23:34:46 +0800

    comment-app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CommentApp from './CommentApp'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <CommentApp />,
+  document.getElementById('root')
+);
